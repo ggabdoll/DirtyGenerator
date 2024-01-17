36cfa9f400de87ee19cdd093c01966219a377af7
ggabdoll <ggabdoll0327@naver.com>
2024-01-18 01:12:46 +0900

    config setup

--- dirtyGenerator/build.gradle.kts
+++ dirtyGenerator/build.gradle.kts
@@ -10,14 +10,16 @@ repositories {
     mavenCentral()
 }
 
+
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.1.4")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("com.intellij.java"/* Plugin Dependencies */))
 }
+
 
 tasks {
     // Set the JVM compatibility versions
